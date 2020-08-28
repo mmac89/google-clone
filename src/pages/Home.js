6d448c38da2d374df1f6f227eab9df357ab60d94
Mathew <mmacelwee89@gmail.com>
2020-08-27 22:44:12 -0700

    initial commit

---
+++ src/pages/Home.js
@@ -0,0 +1,30 @@
+    import React from 'react';
+    import './Home.css';
+    import SearchIcon from '@material-ui/icons/Search';
+    function Home() {
+        return (
+            <div className='home'>
+                <h1>This is the home page</h1>
+
+                
+                <div className='home__header'>
+                    <div className='home__headerLeft'>
+                        {/*Link*/}
+                        {/*Link*/}
+                    </div>
+                    <div className= 'home__headerRight'>
+                        {/*Link*/}
+                        {/*Link*/}
+                        {/*Icon*/}
+                        {/*Avatar*/}
+                    </div>
+                </div>
+
+                <div className='home__body'>
+
+</div>
+ 
+                </div>
+        )      
+    }
+export default Home;
